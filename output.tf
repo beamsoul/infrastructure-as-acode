7de427d4403a6e515bcbe57feaef9310fd672d9c
beamsoul <nurzhanasamidinova@gmail.com>
2019-11-06 18:25:25 -0800

    dgrre

--- output.tf
+++ output.tf
@@ -1,11 +1,11 @@
 output "VPC" {
   value = "${aws_vpc.dev.id}"
 }
-output "public-subnet.id" {
+output "public-subnet" {
   value = "${aws_subnet.public.id}"
 }
 
-output "private-subnet.id" {
+output "private-subnet" {
   value = "${aws_subnet.private.id}"
 }
 
