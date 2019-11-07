d64462894125395753345bfe221b44a89fec593d
beamsoul <nurzhanasamidinova@gmail.com>
2019-11-06 17:58:43 -0800

    push

--- output.tf
+++ output.tf
@@ -6,8 +6,8 @@ output "public_subnet" {
   value = "${aws_subnet.public.id}"
 }
 
-output "private_subnet" {
-  value = "${aws_subnet.id}"
+output "private_subnet." {
+  value = "${aws_subnet.private.id}"
 }
 
 output "aws_sec_group" {
