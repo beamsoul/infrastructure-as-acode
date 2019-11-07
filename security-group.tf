ebc9d2cfb7bb396f121e4f28e39510048c440a86
beamsoul <nurzhanasamidinova@gmail.com>
2019-11-06 18:22:29 -0800

    gfdfg

--- security-group.tf
+++ security-group.tf
@@ -4,6 +4,7 @@ resource "aws_security_group" "public" {
   vpc_id      = "${aws_vpc.dev.id}"
 
   ingress {
+
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
@@ -11,28 +12,25 @@ resource "aws_security_group" "public" {
   }
 
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 443
+    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-    tags = {
-      Name = "${var.Name}.public"
-      Env = "${var.Env}"
-      Created_by = "${var.Created_by}"
-      Dept = "${var.Dept}"
+
+  tags = {
+    Name      = "${var.Name}.public"
+    Env       = "${var.Env}"
+    Create_by = "${var.Created_by}"
+    Dept      = "${var.Dept}"
+
   }
+}
